2b747beacf58d22abe51bd35c1f4790de99e4ac1
unknown <a831202a@gmail.com>
2020-07-05 07:53:46 +0800

    db

---
+++ db/introduction.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2020-07-05 01:52:40
+-- 伺服器版本： 10.4.11-MariaDB
+-- PHP 版本： 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `resume`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `introduction`
+--
+
+CREATE TABLE `introduction` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `introduction`
+--
+
+INSERT INTO `introduction` (`id`, `title`, `text`, `img`) VALUES
+(1, '繼寬的履歷', '擔任過建設公司資訊部資訊人員，主要為軟硬體維護，專案管理。\r\n現在正在積極學習全端網頁開發，並積極希望投身此領域', '');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `introduction`
+--
+ALTER TABLE `introduction`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `introduction`
+--
+ALTER TABLE `introduction`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
